d7fc59c6ce9fd2a21c35fb3d262395c9a2f86d5e
Kishan Nayak <kishannk11@gmail.com>
2023-07-25 00:52:22 +0530

    Navbar Changes

---
+++ client/navbar.php
@@ -0,0 +1,282 @@
+<?php
+?>
+<!DOCTYPE html>
+ <html lang="en">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="x-ua-compatible" content="ie=edge" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+ 
+     <title>Client - Dashboard</title>
+     <meta name="keywords"
+         content="apparel, catalog, clean, ecommerce, ecommerce HTML, electronics, fashion, html eCommerce, html store, minimal, multipurpose, multipurpose ecommerce, online store, responsive ecommerce template, shops" />
+     <meta name="description" content="Best ecommerce html template for single and multi vendor store.">
+     <meta name="author" content="ashishmaraviya">
+ 
+     <!-- site Favicon -->
+     <link rel="icon" href="assets/images/favicon/favicon.png" sizes="32x32" />
+     <link rel="apple-touch-icon" href="assets/images/favicon/favicon.png" />
+     <meta name="msapplication-TileImage" content="assets/images/favicon/favicon.png" />
+ 
+     <!-- css Icon Font -->
+     <link rel="stylesheet" href="assets/css/vendor/ecicons.min.css" />
+ 
+     <!-- css All Plugins Files -->
+     <link rel="stylesheet" href="assets/css/plugins/animate.css" />
+     <link rel="stylesheet" href="assets/css/plugins/swiper-bundle.min.css" />
+     <link rel="stylesheet" href="assets/css/plugins/jquery-ui.min.css" />
+     <link rel="stylesheet" href="assets/css/plugins/countdownTimer.css" />
+     <link rel="stylesheet" href="assets/css/plugins/slick.min.css" />
+     <link rel="stylesheet" href="assets/css/plugins/bootstrap.css" />
+ 
+     <!-- Main Style -->
+     <link rel="stylesheet" href="assets/css/style.css" />
+     <link rel="stylesheet" href="assets/css/responsive.css" />
+ 
+     <!-- Background css -->
+     <link rel="stylesheet" id="bg-switcher-css" href="assets/css/backgrounds/bg-4.css">
+ </head>
+<body class="product_page">
+    <div id="ec-overlay">
+        <div class="ec-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+
+    <!-- Header start  -->
+    <header class="ec-header">
+        <!--Ec Header Top Start -->
+        <!-- Ec Header Top  End -->
+        <!-- Ec Header Bottom  Start -->
+        <div class="ec-header-bottom d-none d-lg-block">
+            <div class="container position-relative">
+                <div class="row">
+                    <div class="ec-flex">
+                        <!-- Ec Header Logo Start -->
+                        <div class="align-self-center">
+                            <div class="header-logo">
+                                <a href="#"><img src="assets/images/logo/legalkarnataka.jpeg" alt="Site Logo" /></a>
+                            </div>
+                        </div>
+                        <!-- Ec Header Logo End -->
+
+                        <!-- Ec Header Search Start -->
+                        <div class="align-self-center">
+                            <div class="header-search">
+                                <form class="ec-btn-group-form" action="#">
+                                    <input class="form-control ec-search-bar" placeholder="Search products..."
+                                        type="text">
+                                    <button class="submit" type="submit"><i class="fi-rr-search"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- Ec Header Search End -->
+
+                        <!-- Ec Header Button Start -->
+                        <div class="align-self-center">
+                            <div class="ec-header-bottons">
+
+                                <!-- Header User Start -->
+                                <div class="ec-header-user dropdown">
+                                    <button class="dropdown-toggle" data-bs-toggle="dropdown"><i
+                                            class="fi-rr-user"></i></button>
+                                    <ul class="dropdown-menu dropdown-menu-right">
+                                        <li><a class="dropdown-item" href="register.html">Register</a></li>
+                                        <li><a class="dropdown-item" href="checkout.html">Checkout</a></li>
+                                        <li><a class="dropdown-item" href="login.html">Login</a></li>
+                                    </ul>
+                                </div>
+                                <!-- Header User End -->
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Ec Header Button End -->
+        <!-- Header responsive Bottom  Start -->
+        <div class="ec-header-bottom d-lg-none">
+            <div class="container position-relative">
+                <div class="row ">
+
+                    <!-- Ec Header Logo Start -->
+                    <div class="col">
+                        <div class="header-logo">
+                            <a href="index.html"><img src="assets/images/logo/logo.png" alt="Site Logo" /><img
+                                    class="dark-logo" src="assets/images/logo/dark-logo.png" alt="Site Logo"
+                                    style="display: none;" /></a>
+                        </div>
+                    </div>
+                    <!-- Ec Header Logo End -->
+                    <!-- Ec Header Search Start -->
+                    <div class="col">
+                        <div class="header-search">
+                            <form class="ec-btn-group-form" action="#">
+                                <input class="form-control ec-search-bar" placeholder="Search products..." type="text">
+                                <button class="submit" type="submit"><i class="fi-rr-search"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- Ec Header Search End -->
+                </div>
+            </div>
+        </div>
+        <!-- Header responsive Bottom  End -->
+        <!-- EC Main Menu Start -->
+        <div id="ec-main-menu-desk" class="d-none d-lg-block sticky-nav">
+            <div class="container position-relative">
+                <div class="row">
+                    <div class="col-md-12 align-self-center">
+                        <div class="ec-main-menu">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li class="dropdown"><a href="javascript:void(0)">Categories</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="product-full-width.html">Affidavat</a></li>
+                                        <li><a href="product-full-width.html">Agreement</a></li>
+                                        <li><a href="product-full-width.html">Deed</a></li>
+                                        <li><a href="product-full-width.html">Will</a></li>
+                                        <li><a href="product-full-width.html">MOU</a></li>
+                                    </ul>
+                                </li>
+
+                                <li class="dropdown"><a href="javascript:void(0)">Orders</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="product-full-width.html">Track Order</a></li>
+                                        <li><a href="product-360-full-width.html">Order History</a></li>
+                                        <li><a href="product-video-full-width.html">Order Details</a></li>
+                                        <li><a href="product-gallery-full-width.html">Invoice</a></li>
+                                    </ul>
+                                </li>
+
+                                <li class="dropdown"><a href="javascript:void(0)">Contact</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="product-full-width.html">Contact Admin</a></li>
+                                        <li><a href="product-360-full-width.html">WhatsApp Admin</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Ec Main Menu End -->
+        <!-- ekka Mobile Menu Start -->
+        <div id="ec-mobile-menu" class="ec-side-cart ec-mobile-menu">
+            <div class="ec-menu-title">
+                <span class="menu_title">My Menu</span>
+                <button class="ec-close"></button>
+            </div>
+            <div class="ec-menu-inner">
+                <div class="ec-menu-content">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+
+                        <li class="dropdown"><a href="javascript:void(0)">Categories</a>
+                            <ul class="sub-menu">
+                                <li><a href="product-full-width.html">Affidavat</a></li>
+                                <li><a href="product-full-width.html">Agreement</a></li>
+                                <li><a href="product-full-width.html">Deed</a></li>
+                                <li><a href="product-full-width.html">Will</a></li>
+                                <li><a href="product-full-width.html">MOU</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown"><a href="javascript:void(0)">Orders</a>
+                            <ul class="sub-menu">
+                                <li><a href="product-full-width.html">Track Order</a></li>
+                                <li><a href="product-360-full-width.html">Order History</a></li>
+                                <li><a href="product-video-full-width.html">Order Details</a></li>
+                                <li><a href="product-gallery-full-width.html">Invoice</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown"><a href="javascript:void(0)">Contact</a>
+                            <ul class="sub-menu">
+                                <li><a href="product-full-width.html">Contact Admin</a></li>
+                                <li><a href="product-360-full-width.html">WhatsApp Admin</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="header-res-lan-curr">
+                    <!-- Social Start -->
+                    <div class="header-res-social">
+                        <div class="header-top-social">
+                            <ul class="mb-0">
+                                <li class="list-inline-item"><a class="hdr-facebook" href="#"><i
+                                            class="ecicon eci-facebook"></i></a></li>
+                                <li class="list-inline-item"><a class="hdr-twitter" href="#"><i
+                                            class="ecicon eci-twitter"></i></a></li>
+                                <li class="list-inline-item"><a class="hdr-instagram" href="#"><i
+                                            class="ecicon eci-instagram"></i></a></li>
+                                <li class="list-inline-item"><a class="hdr-linkedin" href="#"><i
+                                            class="ecicon eci-linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Social End -->
+                </div>
+            </div>
+        </div>
+        <!-- ekka mobile Menu End -->
+    </header>
+    <!-- Header End  -->
+
+    <!-- ekka Cart Start -->
+    <!-- <div class="ec-side-cart-overlay"></div>
+    <div id="ec-side-cart" class="ec-side-cart">
+        <div class="ec-cart-inner">
+            <div class="ec-cart-top">
+                <div class="ec-cart-title">
+                    <span class="cart_title">My Cart</span>
+                    <button class="ec-close">×</button>
+                </div>
+                <ul class="eccart-pro-items">
+                    <li>
+                        <a href="product-left-sidebar.html" class="sidekka_pro_img"><img
+                                src="assets/images/product-image/6_1.jpg" alt="product"></a>
+                        <div class="ec-pro-content">
+                            <a href="product-left-sidebar.html" class="cart_pro_title">T-shirt For Women</a>
+                            <span class="cart-price"><span>$76.00</span> x 1</span>
+                            <div class="qty-plus-minus">
+                                <input class="qty-input" type="text" name="ec_qtybtn" value="1" />
+                            </div>
+                            <a href="javascript:void(0)" class="remove">×</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="product-left-sidebar.html" class="sidekka_pro_img"><img
+                                src="assets/images/product-image/12_1.jpg" alt="product"></a>
+                        <div class="ec-pro-content">
+                            <a href="product-left-sidebar.html" class="cart_pro_title">Women Leather Shoes</a>
+                            <span class="cart-price"><span>$64.00</span> x 1</span>
+                            <div class="qty-plus-minus">
+                                <input class="qty-input" type="text" name="ec_qtybtn" value="1" />
+                            </div>
+                            <a href="javascript:void(0)" class="remove">×</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="product-left-sidebar.html" class="sidekka_pro_img"><img
+                                src="assets/images/product-image/3_1.jpg" alt="product"></a>
+                        <div class="ec-pro-content">
+                            <a href="product-left-sidebar.html" class="cart_pro_title">Girls Nylon Purse</a>
+                            <span class="cart-price"><span>$59.00</span> x 1</span>
+                            <div class="qty-plus-minus">
+                                <input class="qty-input" type="text" name="ec_qtybtn" value="1" />
+                            </div>
+                            <a href="javascript:void(0)" class="remove">×</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div> -->
+    <!-- ekka Cart End -->

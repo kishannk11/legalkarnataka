8ffd1f92b9cf88029bc96ed792abb1a6eae42a0a
6Ro0T <ranjithchandran220@gmail.com>
2023-08-08 13:14:27 +0530

    adding templates

--- admin/Database.php
+++ admin/Database.php
@@ -122,21 +122,102 @@ class Product
         $this->conn = $conn;
     }
 
-    public function addProduct($prodName, $category, $price, $details)
+    public function saveProduct($prod_name, $category, $price, $details, $image)
     {
-        try {
-            $sql = "INSERT INTO products (prod_name, category, price, details) VALUES (:prodName, :category, :price, :details)";
-            $stmt = $this->conn->prepare($sql);
-            $stmt->bindParam(':prodName', $prodName);
-            $stmt->bindParam(':category', $category);
-            $stmt->bindParam(':price', $price);
-            $stmt->bindParam(':details', $details);
-            $stmt->execute();
+        // Sanitize the input data
+        $prod_name = $this->sanitizeInput($prod_name);
+        $category = $this->sanitizeInput($category);
+        $price = $this->sanitizeInput($price);
+        $details = $this->sanitizeInput($details);
+
+        // Validate and process the image file
+        $imagePath = $this->processImage($image);
+
+        // Check if any field is empty
+        if (empty($prod_name) || empty($category) || empty($price) || empty($details) || empty($imagePath)) {
+            return 'Please fill in all the fields.';
+        }
+
+        // Prepare the SQL statement
+        $stmt = $this->conn->prepare("INSERT INTO products (prod_name, category, price, details, image) VALUES (?, ?, ?, ?, ?)");
+
+        // Bind the parameters
+        $stmt->bindParam(1, $prod_name);
+        $stmt->bindParam(2, $category);
+        $stmt->bindParam(3, $price);
+        $stmt->bindParam(4, $details);
+        $stmt->bindParam(5, $imagePath);
+
+        // Execute the statement
+        if ($stmt->execute()) {
+            // Redirect to product-add.php
             return true;
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
+        } else {
+            return 'Error saving the product.';
+        }
+    }
+
+    private function sanitizeInput($input)
+    {
+        // Sanitize the input using appropriate sanitization functions
+        $sanitizedInput = trim($input);
+        $sanitizedInput = stripslashes($sanitizedInput);
+        $sanitizedInput = htmlspecialchars($sanitizedInput);
+        return $sanitizedInput;
+    }
+
+    private function processImage($image)
+    {
+        // Check if a file is uploaded
+        if (!isset($image['tmp_name']) || empty($image['tmp_name'])) {
+            return '';
+        }
+
+        // Check if the file format is valid
+        $allowedFormats = ['jpg', 'jpeg', 'png'];
+        $fileExtension = strtolower(pathinfo($image['name'], PATHINFO_EXTENSION));
+        if (!in_array($fileExtension, $allowedFormats)) {
+            return '';
+        }
+
+        // Generate a unique filename for the image
+        $filename = uniqid() . '.' . $fileExtension;
+
+        // Move the uploaded file to the desired directory
+        $targetDir = 'upload/';
+        $targetFile = $targetDir . $filename;
+        move_uploaded_file($image['tmp_name'], $targetFile);
+
+        return $filename;
+    }
+    public function getProduct()
+    {
+        $product = array();
+
+        $sql = "SELECT * FROM products";
+        $result = $this->conn->query($sql);
+
+        if ($result->rowCount() > 0) {
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                $product[] = $row;
+            }
+        }
+
+        return $product;
+    }
+
+    public function deleteProduct($productId)
+    {
+        $stmt = $this->conn->prepare("DELETE FROM products WHERE id = ?");
+        $stmt->bindParam(1, $productId);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
             return false;
         }
     }
+
+
 }
 ?>

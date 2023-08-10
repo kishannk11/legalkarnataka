f6bfe4bc9dbcaf062f28dce31f88fc836f40cd53
6Ro0T <ranjithchandran220@gmail.com>
2023-08-10 23:53:48 +0530

    updates

--- admin/navbar.php
+++ admin/navbar.php
@@ -121,6 +121,11 @@
 											<span class="nav-text">Sub Category</span>
 										</a>
 									</li>
+									<li class="">
+										<a class="sidenav-item-link" href="#">
+											<span class="nav-text">View All Category</span>
+										</a>
+									</li>
 								</ul>
 							</div>
 						</li>
@@ -171,16 +176,12 @@
 											<span class="nav-text">Order History</span>
 										</a>
 									</li>
-									<!-- <li class="">
-										<a class="sidenav-item-link" href="order-detail.php">
-											<span class="nav-text">Order Detail</span>
+									<li class="">
+										<a class="sidenav-item-link" href="new-order.php">
+											<span class="nav-text">Pending Order</span>
 										</a>
-									</li> -->
-									<!-- <li class="">
-										<a class="sidenav-item-link" href="invoice.php">
-											<span class="nav-text">Invoice</span>
-										</a>
-									</li> -->
+									</li>
+
 								</ul>
 							</div>
 						</li>
@@ -251,11 +252,11 @@
 									<li class="dropdown-header">
 										<img src="assets/img/user/user.png" class="img-circle" alt="User Image" />
 										<div class="d-inline-block">
-											John Deo <small class="pt-1">john.example@gmail.com</small>
+											Admin <small class="pt-1">admin@gmail.com</small>
 										</div>
 									</li>
 									<li>
-										<a href="user-profile.html">
+										<a href="user-profile.php">
 											<i class="mdi mdi-account"></i> My Profile
 										</a>
 									</li>
@@ -264,14 +265,9 @@
 											<i class="mdi mdi-email"></i> Message
 										</a>
 									</li>
-									<li>
-										<a href="#"> <i class="mdi mdi-diamond-stone"></i> Projects </a>
-									</li>
-									<li class="right-sidebar-in">
-										<a href="javascript:0"> <i class="mdi mdi-settings-outline"></i> Setting </a>
-									</li>
+
 									<li class="dropdown-footer">
-										<a href="index.html"> <i class="mdi mdi-logout"></i> Log Out </a>
+										<a href="#"> <i class="mdi mdi-logout"></i> Log Out </a>
 									</li>
 								</ul>
 							</li>
--- client/login_otp.php
+++ client/login_otp.php
@@ -88,30 +88,27 @@ if (isset($_GET['error'])) {
 
                     </div>
                     <div class="card-body p-5">
-                        <h4 class="text-dark mb-5">Sign In</h4>
+                        <h4 class="text-dark mb-5">Login With OTP</h4>
 
-                        <form method="POST" class="form-horizontal auth-form my-4" action="user_login.php">
+                        <form method="POST" class="form-horizontal auth-form my-4" action="">
                             <div class="row">
                                 <div class="form-group col-md-12 mb-4">
-                                    <input type="text" class="form-control" id="email" name="email"
-                                        placeholder="Username">
+                                    <input type="tel" class="form-control" id="email" name="phonenumber"
+                                        placeholder="Enter Phone Number">
                                 </div>
 
                                 <div class="form-group col-md-12 ">
                                     <input type="password" class="form-control" id="password" name="password"
-                                        placeholder="Password">
+                                        placeholder="Enter OTP">
                                 </div>
 
                                 <div class="col-md-12">
                                     <div class="d-flex my-2 justify-content-between">
                                         <div class="d-inline-block mr-3">
-                                            <div class="control control-checkbox">Remember me
-                                                <input type="checkbox" />
-                                                <div class="control-indicator"></div>
-                                            </div>
+
                                         </div>
 
-                                        <p><a class="text-blue" href="#">Forgot Password?</a></p>
+
                                     </div>
 
                                     <button type="submit" class="btn btn-primary btn-block mb-4">Sign In</button>
@@ -119,9 +116,7 @@ if (isset($_GET['error'])) {
                                     <p class="sign-upp">Don't have an account yet ?
                                         <a class="text-blue" href="register.php">Sign Up</a>
                                     </p>
-                                    <p class="sign-upp">Login With OTP
-                                        <a class="text-blue" href="login_otp.php">Sign In With OTP</a>
-                                    </p>
+
                                 </div>
                             </div>
                         </form>

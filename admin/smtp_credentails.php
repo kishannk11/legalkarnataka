2ed44f713fb55a03409b200496ada4ec191f0078
6Ro0T <ranjithchandran220@gmail.com>
2023-11-06 13:31:28 +0530

    bug fixes

--- admin/smtp_credentails.php
+++ admin/smtp_credentails.php
@@ -2,5 +2,5 @@
 define('SMTP_HOST', 'smtp.hostinger.com');
 define('SMTP_PORT', '465');
 define('SMTP_USERNAME', 'support@legalkarnataka.com');
-define('SMTP_PASSWORD', 'G2PjmbtuD8z4665!!%r');
+define('SMTP_PASSWORD', 'xb8ui4PcGxCMh8g!!');
 ?>
